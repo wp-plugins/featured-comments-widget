15ee3387882c0247ce7c1a23488caa18c36c34aa
andreu <andreu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-17 21:30:35 +0000

    Spanish language added
    git-svn-id: https://plugins.svn.wordpress.org/featured-comments-widget/trunk@613816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- featured-comments-widget.php
+++ featured-comments-widget.php
@@ -7,6 +7,7 @@ Version: 1.0
 Author: Andreu Llos
 Author URI: http://andreullos.com
 Copyright: 2012, Andreu Llos
+Text Domain: featured-comments-widget
 
 GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
 This program is free software; you can redistribute it and/or modify
@@ -27,8 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function featured_comments_textdomain() {
 	if ( function_exists('load_plugin_textdomain') ) {
-		//load_plugin_textdomain('featured_comments', false, dirname(plugin_basename(__FILE__)).'/languages/');
-		load_plugin_textdomain('featured_comments', false, basename( dirname( __FILE__ ) ) . '/languages' );
+		load_plugin_textdomain('featured-comments-widget', false, basename( dirname( __FILE__ ) ) . '/languages' );
 
 	}
 }
@@ -38,9 +38,9 @@ add_action( 'init', 'featured_comments_textdomain' );
 class featured_comments_widget extends WP_Widget {
     //process our new widget
     function featured_comments_widget() {
-        $widget_ops = array('classname' => 'widget_featured_comments', 'description' => __('Display the comments you want to feature','featured_comments')); 
+        $widget_ops = array('classname' => 'widget_featured_comments', 'description' => __('Display the comments you want to feature','featured-comments-widget')); 
         $control_ops = array( 'width' => 550 );
-        $this->WP_Widget('widget_featured_comments', __('Featured Comments','featured_comments'), $widget_ops, $control_ops);
+        $this->WP_Widget('widget_featured_comments', __('Featured Comments','featured-comments-widget'), $widget_ops, $control_ops);
         
 	    function get_featured_comments_short_text_words($text, $limit=50) {
 	    	$excerpt = explode(' ', $text, $limit);
@@ -58,24 +58,24 @@ class featured_comments_widget extends WP_Widget {
        
     //build our widget settings form
     function form($instance) {
-        $defaults = array('title' => __('Featured Comments','featured_comments'), 'count' => 5,'words' => 20); 
+        $defaults = array('title' => __('Featured Comments','featured-comments-widget'), 'count' => 5,'words' => 20); 
         $i=1; while($i<20){ $defaults['comment_'.$i] = '';  $i++; }
         $instance = wp_parse_args( (array) $instance, $defaults );
         ?>
-        	<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title', 'featured_comments'); ?>:</label>
+        	<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title', 'featured-comments-widget'); ?>:</label>
         	<input type="text" name="<?php echo $this->get_field_name('title'); ?>" class="widefat" value="<?php echo esc_attr($instance['title']); ?>" /></p>
 
-            <p><label for="<?php echo $this->get_field_id( 'count' ); ?>"><?php _e('Number Of Comments To Show', 'featured_comments'); ?>:</label>
+            <p><label for="<?php echo $this->get_field_id( 'count' ); ?>"><?php _e('Number Of Comments To Show', 'featured-comments-widget'); ?>:</label>
         	<select class="widefat" id="<?php echo $this->get_field_id( 'count' ); ?>" name="<?php echo $this->get_field_name('count'); ?>"><? $i=0; while($i<20){ $i++; ?>
-        	<option value="<? print $i; ?>"<? if($instance['count']==$i){ ?>selected="selected"<? } ?>><? print $i.' '.__('comments','featured_comments'); ?></option><? } ?>
+        	<option value="<? print $i; ?>"<? if($instance['count']==$i){ ?>selected="selected"<? } ?>><? print $i.' '.__('comments','featured-comments-widget'); ?></option><? } ?>
    			</select></p>
    			
-   			<p><label for="<?php echo $this->get_field_id( 'words' ); ?>"><?php _e('Number Of Words Per Comment', 'featured_comments'); ?>:</label>
+   			<p><label for="<?php echo $this->get_field_id( 'words' ); ?>"><?php _e('Number Of Words Per Comment', 'featured-comments-widget'); ?>:</label>
         	<select class="widefat" id="<?php echo $this->get_field_id( 'words' ); ?>" name="<?php echo $this->get_field_name('words'); ?>"><? $i=0; while($i<200){ $i++; ?>
-        	<option value="<? print $i; ?>"<? if($instance['words']==$i){ ?>selected="selected"<? } ?>><? print $i.' '.__('words','featured_comments'); ?></option><? } ?>
+        	<option value="<? print $i; ?>"<? if($instance['words']==$i){ ?>selected="selected"<? } ?>><? print $i.' '.__('words','featured-comments-widget'); ?></option><? } ?>
    			</select></p>
    				    
-		    <h4><?php _e('Select Featured Comments', 'featured_comments'); ?></h4>
+		    <h4><?php _e('Select Featured Comments', 'featured-comments-widget'); ?></h4>
 		    <ul>
 		    <? $i=1; while($i<=$instance['count']){ ?>
 		    <li><select style="width:390px" id="<?php echo $this->get_field_id( 'comment_'.$i ); ?>" name="<?php echo $this->get_field_name('comment_'.$i); ?>">
@@ -124,7 +124,7 @@ class featured_comments_widget extends WP_Widget {
 		$i=1; while($i<$count){ 
 			if(${'comment_'.$i}!=''){
 				$comment = get_comment(${'comment_'.$i});
-				echo '<li><span class="comment-meta"><span class="author">'.$comment->comment_author.'</span> '.__('in','featured_comments').' <span class="comment-title"><a href="'.get_permalink($comment->comment_post_ID).'">'.get_the_title($comment->comment_post_ID).'</a></span>:</span><blockquote>'.get_featured_comments_short_text_words($comment->comment_content,$words).'</blockquote></li>';	
+				echo '<li><span class="comment-meta"><span class="author">'.$comment->comment_author.'</span> '.__('on','featured-comments-widget').' <span class="comment-title"><a href="'.get_permalink($comment->comment_post_ID).'">'.get_the_title($comment->comment_post_ID).'</a></span>:</span><blockquote>'.get_featured_comments_short_text_words($comment->comment_content,$words).'</blockquote></li>';	
 			}
 			$i++;
 		}
